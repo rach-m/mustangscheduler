d1cdb5f95b556ab6303b6f87f96c6a8bab549ab9
= <azaria.david@gmail.com>
2018-03-12 13:49:39 -0400

    adding easter eggs

--- easter-eggs.js
+++ easter-eggs.js
@@ -82,7 +82,7 @@ module.exports = function(bot, taID) {
       }
     }
     cb(null, 'floorMessage');
-  }
+  };
 
   var favoriteThings = function(message, cb) {
     if (validate(message)) {
@@ -94,7 +94,7 @@ module.exports = function(bot, taID) {
       bot.sendMessage(message.channel, botMessage);
     }
     cb(null, 'favoriteThings');
-  }
+  };
 
   var doYouLike = function(message, cb) {
     if (validate(message)) {
@@ -106,7 +106,7 @@ module.exports = function(bot, taID) {
       bot.sendMessage(message.channel, botMessage);
     }
     cb(null, 'doYouLike');
-  }
+  };
 
   var thanks = function(message, cb) {
     if (validate(message)) {
@@ -121,7 +121,7 @@ module.exports = function(bot, taID) {
       bot.sendMessage(message.channel, botMessage);
     }
     cb(null, 'thanks');
-  }
+  };
 
 	 var howAwesome = function(message, cb) {
     if (validate(message)) {
@@ -132,7 +132,7 @@ module.exports = function(bot, taID) {
       bot.sendMessage(message.channel, botMessage);
     }
     cb(null, 'howAwesome');
-  }
+  };
 
 	var wakeUp = function(message, cb) {
 		if (validate(message)) {
@@ -143,7 +143,7 @@ module.exports = function(bot, taID) {
 			bot.sendMessage(message.channel, botMessage);
 		}
 		cb(null, 'wakeUp');
-	}
+	};
 
 	var theDom = function(message, cb) {
 		if (validate(message)) {
@@ -154,7 +154,7 @@ module.exports = function(bot, taID) {
 			bot.sendMessage(message.channel, botMessage);
 		}
 		cb(null, 'theDom');
-	}
+	};
 
 	const virtualDom = (message, cb) => {
 		if (validate(message)) {
@@ -166,7 +166,7 @@ module.exports = function(bot, taID) {
 			bot.sendMessage(message.channel, botMessage);
 		}
 		cb(null, 'virtualDom');
-	}
+	};
 
 	const heart = (message, cb) => {
 			if (validate(message)) {
@@ -178,8 +178,102 @@ module.exports = function(bot, taID) {
 			bot.sendMessage(message.channel, botMessage);
 		}
 		cb(null, 'heart');
-	}
+	};
 
+    var waterBoy = function(message, cb) {
+      if (validate(message)) {
+        let answers = [
+        "Louisiana was named in honor of King Louis XIV, the King of France from 1643-1715.",
+        "Just because it’s called the “French Quarter” doesn’t mean that being in New Orleans’ famous neighborhood is like strolling through a Parisian city. Most of the buildings today were influenced by Spanish architecture after a fire in 1794 destroyed most of the French colonial architecture.",
+        "Louisiana is the only state that still acts under Napoleonic code, which derives from the original French emperor’s civil code.",
+        "There are almost half as many alligators as there are people in Louisiana.",
+        "Mardi Gras may be the most well-known celebration in Louisiana, but the festival’s roots can be traced back to medieval Europe.",
+        "Speaking of Mardi Gras, each official color has an assigned meaning. Purple stands for justice, gold for power and green for faith.",
+        "Baton Rouge is the only site of the American Revolution Battle that was fought outside the original 13 colonies.",
+        "Louisiana is the only state in the union that doesn’t have counties. Instead, its political subdivisions are called parishes.",
+        "Louisiana-made tabasco sauce holds the second oldest food trademark in the US patent office.",
+        "Louisiana is one of the few US states that offers sales tax refunds to international visitors.",
+        "Louisiana is the largest producer of alligators, crawfish and oysters in the country.",
+        "Canal Street, one of New Orleans’ busiest streets, was named after a canal that was supposed to be built there. However, the waterway was never dug.",
+        "The official beverage of Louisiana isn’t a Hurricane, Sazarec or any other signature alcoholic drink. It’s milk.",
+        "Louisiana has its own French dialects, the most popular being Creole French. The dialect is mainly a mix of the original French spoken by settlers and Acadian French, but it also includes words of African, Spanish, Native American and English descent."
+        ];
+        var command = paramify(message);
+        if ((command[0] === "Tell" || command[0] === "tell") &&
+          command[1] === "me" &&
+          command[2] === "something" &&
+          command[3] === "fun" &&
+          command[4] === "about" &&
+          (command[5] === "louisiana" || command[5] === "louisiana?")) {
+          var botMessage =  answers[Math.floor(Math.random() * answers.length)];
+          console.log("hey look at me now!!!!!!", botMessage);
+        }
+
+        bot.sendMessage(message.channel, botMessage);
+      }
+      cb(null, 'waterBoy');
+    };
+
+    var tellMeSomething = function(message, cb) {
+    if(validate(message)) {
+      let factsArray = [
+        "When you're playing a video game that requires stealth, the enemy/AI know exactly where you are at all times, but are forced to not interact with you until one of your actions in-game trigger a line of code.",
+        "If Pinocchio said the statement 'My nose will grow now', it would cause a paradox because if his nose grows that would mean he wasn't lying and so his nose shouldn't have grown, but if it doesn't grow, that would mean his statement was a lie and his nose should have grown.",
+        "The number `14233221` describes itself; it has one four, two threes, three twos, and two ones.",
+        "If 1 human year = 7 dog years, that means 1 human day = 7 dog days. Which explains why they're so excited to see you after a long day. To them, it's been a week.",
+        "Scooby Doo is basically a show that teaches kids that the villains in our lives are usually not strangers, but rather people we know and trust.",
+        "The word 'crisp' starts at the back of your mouth and ends at the front.",
+        "The letter 'W' starts with a 'D'.",
+        "Every odd number that exists has the letter 'E' in it.",
+        "Your phone doesn't auto-correct when you're typing in all caps because it thinks you are extremely angry and doesn't want to get involved.",
+        "Your future self is watching you right now through memories.",
+        "A pizza is a circle that comes in a square box that we eat in a triangle.",
+        "Blood is thicker than water, but maple syrup is thicker than blood.  Therefore, pancakes are more important than family.",
+        "'Finally' is pronounced 'final-e', but 'finale' is pronounced 'fi-nally'.",
+        "Lead rhymes with read. Lead rhymes with read.",
+        "Cleopatra lived closer in time to the moon landing than the construction of the Great Pyramids.",
+        "One million seconds is 11 days. One billion seconds is 31 years.",
+        "North Korea and Finland are separated by only one country: Russia.",
+        "If your car could drive straight up, outer space is only an hour drive away.",
+        "The Nintendo Corporation was created before the fall of the Ottoman Empire.",
+        "You have never seen your face, only pictures and reflections.",
+        "If you add up the total amount of hours people have played World of Warcraft, it would come out to 6.8 million years.",
+        "The surface area of Russia is slightly larger than the surface area of Pluto.",
+        "Anne Frank and Martin Luther King Jr. were born in the same year.",
+        "High school seniors who graduated in the class of 2015 had never been alive while The Simpsons was not on TV.",
+        "The population of Mars consists entirely of robots.",
+        "Oxford University is older than the Aztec Empire.",
+        "There are more fake flamingos in the world than real flamingos.",
+        "New York City is further south than Rome, Italy.",
+        "Mammoths went extinct 1,000 years after the Egyptians finished building the Great Pyramid.",
+        "Carrots were originally purple.",
+        "An octopus has three hearts.",
+        "There's enough water in Lake Superior to cover all of North and South America in 1 foot of water.",
+        "There is a basketball court on the top floor of the U.S. Supreme Court Building known as the 'highest court in the land'.",
+        "'umop apisdn' is 'upside down' spelled upside down.",
+        "The name 'Jessica' was created by Shakespeare in the play _Merchant of Venice_.",
+        "If you put your finger in your ear and scratch, it sounds just like Pac-Man.",
+        "Every year, hundreds of new trees grow because of squirrels forgetting where they buried their nuts.",
+        "Alaska is simultaneously the most northern, the most western, and the most eastern state in the U.S.",
+        "Vending machines are twice as likely to kill you as a shark is.",
+        "Will Smith is now older than Uncle Phil was at the beginning of _The Fresh Prince of Bel-Air_.",
+        "There is less time between the existence of the Tyrannosaurus Rex and humans than there was between the Tyrannosaurus Rex and the Stegosaurus."
+      ];
+      var command = paramify(message);
+      if(
+          (command[0] === "Tell" || command[0] === "tell") &&
+          (command[1] === "me") &&
+          (command[2] === "something") &&
+          (command[3] === "I" || command[3] === "i") &&
+          (command[4] === "don't" || command[4] === "dont" || command[4] === "don’t") &&
+          (command[5] === "know")
+      ) {
+        var botMessage = factsArray[Math.floor(Math.random() * factsArray.length)];
+      }
+      bot.sendMessage(message.channel, botMessage)
+    }
+    cb(null, 'tellMeSomething');
+  };
 
 	return {
 		quoteMachine,
@@ -192,7 +286,9 @@ module.exports = function(bot, taID) {
 		wakeUp,
 		theDom,
 		virtualDom,
-		heart
+		heart,
+    waterBoy,
+    tellMeSomething
 	};
 
 }; // module.exports
